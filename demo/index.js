6a90bb600fc40bdfb4f0d763c64dc4d3b926f71f
Brett Zamir <brettz9@yahoo.com>
2023-04-21 13:01:21 -0700

    docs: DOM element API

--- demo/index.js
+++ demo/index.js
@@ -9,10 +9,20 @@ jml('section', {role: 'main'}, [
   ['h2', [
     'Key path expected (object required at root)'
   ]],
+
+  // First element returned has:
+  //   $setFormat(valueFormat) - Sets format and rebuilds type choices
+  //   $buildTypeChoices() - Rebuilds type choices
+
+  // Second element returned (typesHolder) has two simple DOM-getting methods
+  //   for retrieving items within the types holder:
+  //   $getTypeRoot()
+  //   $getTypeSelect()
   ...typeChoices({
     hasKeyPath: true,
     typeNamespace: 'demo-keypath-expected'
   }),
+
   ['h2', [
     'No key path expected (type can vary at root)'
   ]],
@@ -20,6 +30,7 @@ jml('section', {role: 'main'}, [
     hasKeyPath: false,
     typeNamespace: 'demo-keypath-not-expected'
   }),
+
   ['h2', [
     'Format choices without type selection ' +
       '(might use as retrieval return format)'
