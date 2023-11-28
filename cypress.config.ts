0e46096d06d940424b323000b2d2e6a6e8b9ef04
Brett Zamir <brettz9@yahoo.com>
2023-11-28 03:19:21 -0700

    chore: Cypress Cloud

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ import plugins from './cypress/plugins/index.js';
 export default defineConfig({
   defaultCommandTimeout: 8000,
   video: false,
+  projectId: 'biz82n', // Cypress Cloud
   e2e: {
     baseUrl: 'http://127.0.0.1:8087',
     setupNodeEvents (on, config) {
