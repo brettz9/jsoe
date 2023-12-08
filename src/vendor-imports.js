c247d60b75799e2417c7b28a46149865ee95b372
Brett Zamir <brettz9@yahoo.com>
2023-12-07 18:54:36 -0700

    refactor: TS work

--- src/vendor-imports.js
+++ src/vendor-imports.js
@@ -1,7 +1,7 @@
-// export {jml, body, nbsp, $} from 'jamilih';
-export {jml, body, nbsp, $} from '../node_modules/jamilih/dist/jml-es.js';
+export {jml, body, nbsp, $} from 'jamilih';
+// export {jml, body, nbsp, $} from '../node_modules/jamilih/dist/jml-es.js';
 
 export {
   getJSONType, Typeson, unescapeKeyPathComponent, structuredCloningThrowing
-// } from 'typeson-registry';
-} from '../node_modules/typeson-registry/dist/index.js';
+} from 'typeson-registry';
+// } from '../node_modules/typeson-registry/dist/index.js';
