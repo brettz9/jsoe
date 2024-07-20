febb03ef92941e862c0226e36f649d64e4163dee
Brett Zamir <brettz9@yahoo.com>
2024-07-21 07:00:27 +0800

    fix: undo buggy Rollup upgrade

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -3,6 +3,10 @@
 module.exports = {
   reject: [
     // todo[engine:node@>=20]: Can remove
-    'rimraf'
+    'rimraf',
+
+    // todo: check for updates that may fix, but current 4.19.0 causes problems
+    //        for instrumenting (the index-schema.html file won't load)
+    'rollup'
   ]
 };
