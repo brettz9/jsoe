8e23f6ad69b19824db3bdc33674812073a79c1a5
Brett Zamir <brettz9@yahoo.com>
2024-08-07 08:24:52 +0800

    fix: new API usage fix

--- demo/index-instrumented.js
+++ demo/index-instrumented.js
@@ -523,21 +523,21 @@ setTimeout(async function () {
       'Convert arbitrary value to an editable menu'
     ]],
 
-    (await types.getControlsForFormatAndValue(
+    await types.getControlsForFormatAndValue(
       'structuredCloning',
       new Date('1999-01-01')
-    )).rootUI,
+    ),
 
     ['h2', [
       'Convert arbitrary value to a readonly menu'
     ]],
 
-    (await types.getControlsForFormatAndValue(
+    await types.getControlsForFormatAndValue(
       'structuredCloning',
       new Date('1999-01-01'), {
         readonly: true
       }
-    )).rootUI,
+    ),
 
     ['h2', [
       'Convert structured cloning string representation to value and log'
