83fb95b00b807c392eba7c328f4c7c6d011a30db
Brett Zamir <brettz9@yahoo.com>
2024-08-09 16:07:40 +0800

    chore: update rollup

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -3,10 +3,6 @@
 module.exports = {
   reject: [
     // todo[engine:node@>=20]: Can remove
-    'rimraf',
-
-    // todo: check for updates that may fix, but current 4.19.0 causes problems
-    //        for instrumenting (the index-schema.html file won't load)
-    'rollup'
+    'rimraf'
   ]
 };
